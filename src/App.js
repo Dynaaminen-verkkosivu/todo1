468f911d4d8053bd2cc8cae3eade3645c823cd21
Pietari Tanner <cotapi01@students.oamk.fi>
2021-10-12 19:54:56 +0300

    Lisäys

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
-const URL = 'http://localhost/todo/index.php'
+const URL = 'http://localhost/todo/';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
 
   useEffect(() => {
     axios.get(URL)
@@ -16,9 +17,31 @@ function App() {
       })
     
   }, [])
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data]);
+      setTask('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    });
+  }
+
   return (
     <div className="container">
       <h3>Todo List</h3>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={task} onChange={e => setTask(e.target.value)} />
+        <button>Save</button>
+      </form>
       <ol>
         {tasks?.map(task => (
           <li key={task.id}>{task.description}</li>
